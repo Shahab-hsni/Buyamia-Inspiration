223eead7efdb8dc3ee74fff2ad036d3e9569bc3f
Nil Mistry <nil.fahm@gmail.com>
2023-06-22 11:47:13 +0530

    Changes

--- src/service/httpBase.js
+++ src/service/httpBase.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpBase = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/`,
+    baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
     headers: {
         "Content-type": "application/json",
     },
